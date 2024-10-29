367254915477de5da6225c66602c4c9c8d0855b0
Jyoti Bhayana <jbhayana@google.com>
2024-10-28 21:43:29 -0700

    Add multi-client support in camera2
    Add support for multiple clients to access the same camera
    using camera2 api.
    
    Bug:265196098
    Test: Tested that a java and native client are able to open the camera
    at the same time and get the required callbacks and they are able to get
    the shared session configuration using newly added characteristics.
    Tested the clientaccessprioritieschanged callback occur appropriately
    when new client connects/disconnects.
    
    Change-Id: I988ab22c6c9bc474ef264b8e3ff94ea08fdf9419

--- camera/src/camera_metadata_asserts.cpp
+++ camera/src/camera_metadata_asserts.cpp
@@ -188,6 +188,8 @@ static_assert(static_cast<int>(ANDROID_EXTENSION)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSection::ANDROID_EXTENSION));
 static_assert(static_cast<int>(ANDROID_JPEGR)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSection::ANDROID_JPEGR));
+static_assert(static_cast<int>(ANDROID_SHARED_SESSION)
+        == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSection::ANDROID_SHARED_SESSION));
 static_assert(static_cast<int>(VENDOR_SECTION)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSection::VENDOR_SECTION));
 
@@ -259,6 +261,8 @@ static_assert(static_cast<int>(ANDROID_EXTENSION_START)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSectionStart::ANDROID_EXTENSION_START));
 static_assert(static_cast<int>(ANDROID_JPEGR_START)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSectionStart::ANDROID_JPEGR_START));
+static_assert(static_cast<int>(ANDROID_SHARED_SESSION_START)
+        == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSectionStart::ANDROID_SHARED_SESSION_START));
 static_assert(static_cast<int>(VENDOR_SECTION_START)
         == static_cast<int>(::aidl::android::hardware::camera::metadata::CameraMetadataSectionStart::VENDOR_SECTION_START));
 
